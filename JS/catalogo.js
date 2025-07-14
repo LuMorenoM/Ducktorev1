3a43d57c702bdcf59156819bee06c9cbd5ebb129
Mio <gusw.0246.lpsyg@gmail.com>
2025-07-14 09:37:13 +0200

    crear indormaciones de patos

---
+++ JS/catalogo.js
@@ -0,0 +1,17 @@
+//import {patos} from "../JS/datosdepatos";
+
+/*1.
+//write directly to html
+const title = document.getElementById("hola");
+
+title.textContent = "hola soy freddie";
+*/
+
+/*
+//2. get infomation from datosdepatos.js 
+const title = document.getElementById("hola");
+title.textContent = patos[0].nombre; //-> show:  Freddie
+   //get images from DB
+const img = document.getElementById("imgfreddie");
+img.src = patos[0].imagen;
+*/
