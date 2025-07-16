ad154be3f7ae2fa476faa4553f14d3fecaf94350
Mio <gusw.0246.lpsyg@gmail.com>
2025-07-16 16:42:56 +0200

    Arreglado conflicto

--- JS/filter.js
+++ JS/filter.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.querySelector(".btnFilter");
-    const category = document.querySelector(".category");
-
-    btn.addEventListener("click", () => {
-      category.classList.toggle("active");
-    });
-});
 const optionColor = document.querySelectorAll(".opcionColor");
 const optionCategoria = document.querySelectorAll(".opcionCategoria");
 const optionTamaño = document.querySelectorAll(".opcionTamaño");
